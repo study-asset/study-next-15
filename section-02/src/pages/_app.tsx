27ee84b0d54b2933ecdd967452ec025dfed79e90
Jiwoo So <jiwooproity@naver.com>
2025-06-21 00:15:07 +0900

    Doc(global-layout): 글로벌 레이아웃 설정하기

--- section-02/src/pages/_app.tsx
+++ section-02/src/pages/_app.tsx
@@ -1,35 +1,12 @@
+import GlobalLayout from "@/components/global-layout";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  const onClick = () => {
-    router.push("/test");
-  };
-
-  useEffect(() => {
-    router.prefetch("/test");
-  }, []);
-
   return (
-    <>
-      <header>
-        <Link href={"/"}>Index</Link>
-        &nbsp;
-        <Link href={"/search?q=1"} prefetch={false}>
-          Search
-        </Link>
-        &nbsp;
-        <Link href={"/book/1"}>Book</Link>
-        <div>
-          <button onClick={onClick}>/test 페이지</button>
-        </div>
-      </header>
+    <GlobalLayout>
       <Component {...pageProps} />
-    </>
+    </GlobalLayout>
   );
 }
