d2c7dd2a5ea30a163995b5850d6d14b93a6e432f
Jiwoo So <jiwooproity@naver.com>
2025-06-22 19:49:28 +0900

    Fix(modal): modal과 bookpage 병렬 처리 - @modal 파일에 가로챌 book/[id] 폴더를 만들어 부모 layout.tsx와 엮어주었다. - 엮어진 modal은 layout에서 props로 가져와 children과 병렬로 렌더링되도록 처리했다. - 처리된 modal은 기본 페이지 파일이 존재하지 않아 엮어진 페이지는 404가 뜨기에 default.tsx로 null 값을 반환하여 404를 방지했다.

--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -27,8 +27,10 @@ async function Footer() {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -40,6 +42,7 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </div>
+        {modal}
         <div id="modal-root"></div>
       </body>
     </html>
